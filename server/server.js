dfdaecddf8ee200816699fb39d8cf951a9c4325d
Atharv Deshmukh <atharvdeshmukhad@gmail.com>
2025-08-07 03:00:34 +0530

    Frontend Homepage1

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
 // Entry point for the backend
+const express = require("express");
+const cors = require("cors");
+const apiroutes = require("./routes/index");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api", apiroutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
